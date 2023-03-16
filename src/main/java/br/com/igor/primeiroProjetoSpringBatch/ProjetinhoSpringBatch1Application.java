bf65a775d9adcff8c2ddb2afdc93abb3652519ba
mjigormj <igorcruzsilva17@gmail.com>
2023-03-16 15:54:16 -0300

    first commit

---
+++ src/main/java/br/com/igor/primeiroProjetoSpringBatch/ProjetinhoSpringBatch1Application.java
@@ -0,0 +1,13 @@
+package br.com.igor.primeiroProjetoSpringBatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetinhoSpringBatch1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetinhoSpringBatch1Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/igor/primeiroProjetoSpringBatch/ProjetinhoSpringBatch1ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.igor.primeiroProjetoSpringBatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetinhoSpringBatch1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
